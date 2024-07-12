31e6646dc506b69178c5eed30013fe20b7301f86
pandora-1 <michaelkahari.mk@gmail.com>
2024-07-13 04:32:11 +0700

    feat: edit photo navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -133,7 +133,7 @@ export default function Navbar({ hidden = false }) {
                       <span className="sr-only">Open user menu</span>
                       <img
                         alt=""
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         className="h-8 w-8 rounded-full"
                       />
                     </MenuButton>
