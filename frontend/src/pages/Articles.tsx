3a4251b1e0bd910c42dbe3a32e9622036be891ac
pandora-1 <michaelkahari.mk@gmail.com>
2024-07-12 16:07:41 +0700

    feat: create articles

--- frontend/src/pages/Articles.tsx
+++ frontend/src/pages/Articles.tsx
@@ -1,75 +1,208 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-interface Article {
-  title: string;
-  link: string;
-  snippet: string;
-  image: string;
-}
-
-async function fetchNews(): Promise<Article[]> {
-  const apiKey =
-    "6790a17acf5e76fef8e6608c0767fff189888fde1efa3c35be34dc70ca566ee1";
-
-  const params = {
-    engine: "google_news",
-    api_key: apiKey,
-    q: "scholarship",
-  };
-
-  try {
-    const response = await axios.get(`https://serpapi.com/search.json`, {
-      params,
-    });
-    console.log(response.data);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    return [];
+const posts = [
+  {
+    "position": 1,
+    "title": "UNAIR Medical school graduate secures LPDP scholarship at Harvard Medical School",
+    "source": {
+      "name": "Beranda UNAIR",
+      "icon": "https://encrypted-tbn1.gstatic.com/faviconV2?url=https://unair.ac.id&client=NEWS_360&size=96&type=FAVICON&fallback_opts=TYPE,SIZE,URL"
+    },
+    "link": "https://unair.ac.id/en/unair-medical-school-graduate-secures-lpdp-scholarship-at-harvard-medical-school/",
+    "thumbnail": "https://unair.ac.id/wp-content/uploads/2024/05/Foto-Annisa-Salsabila-Dwi-Nugrahani-saat-menghadiri-21st-Japanese-Society-of-Medical-Oncology-JSMO-pada-22-hingga-24-Februari-2024-di-Nagoya-Congress-Center-Jepang.-Foto-Dok-Pribadi-2-225x300.webp",
+    "date": "07/11/2024, 01:03 AM, +0000 UTC"
+  },
+  {
+    "position": 2,
+    "title": "Embassy of the Republic of Indonesia in Algiers, The People's Democratic Republic of Algeria",
+    "source": {
+      "name": "Kementerian Luar Negeri",
+      "icon": "https://encrypted-tbn0.gstatic.com/faviconV2?url=https://kemlu.go.id&client=NEWS_360&size=96&type=FAVICON&fallback_opts=TYPE,SIZE,URL"
+    },
+    "link": "https://kemlu.go.id/algiers/en/news/27746/knb-scholarship-2024-officially-opens-for-registration",
+    "thumbnail": "https://backpanel.kemlu.go.id/PublishingImages/24.1.21%20-%20Portal.png",
+    "date": "02/01/2024, 02:59 AM, +0000 UTC"
+  },
+  {
+    "position": 3,
+    "title": "Five METCO grads win inaugural Ellen Garrison Scholarship",
+    "source": {
+      "name": "Concord Bridge",
+      "icon": "https://encrypted-tbn0.gstatic.com/faviconV2?url=https://concordbridge.org&client=NEWS_360&size=96&type=FAVICON&fallback_opts=TYPE,SIZE,URL"
+    },
+    "link": "https://concordbridge.org/index.php/2024/07/11/five-metco-grads-win-inaugural-ellen-garrison-scholarship/",
+    "thumbnail": "https://concordbridge.org/wp-content/uploads/2024/05/RobbinsHouse_PANEL_1_Meet-Ellen_22x70.jpeg",
+    "date": "07/11/2024, 07:27 PM, +0000 UTC"
+  },
+  {
+    "position": 4,
+    "title": "ASUU gives N400,000 scholarship to Ebonyi indigent students",
+    "source": {
+      "name": "Punch Newspapers",
+      "icon": "https://lh3.googleusercontent.com/dlvY8oA3iiZX8VsgsdCp9ycitXWle_-ACthuhotArtBJRq1_aYfgyuwT3L5WRAvKeZoWouwvHTk",
+      "authors": [
+        "Edward Nnachi"
+      ]
+    },
+    "link": "https://punchng.com/asuu-gives-n400000-scholarship-to-ebonyi-indigent-students/",
+    "thumbnail": "https://cdn.punchng.com/wp-content/uploads/2018/10/03060033/ASUU-logo.jpg",
+    "date": "07/12/2024, 07:43 AM, +0000 UTC"
+  },
+  {
+    "position": 5,
+    "title": "Ford Family Scholarship gives out $50,000 to Penhold students",
+    "source": {
+      "name": "The Albertan",
+      "icon": "https://encrypted-tbn2.gstatic.com/faviconV2?url=https://www.thealbertan.com&client=NEWS_360&size=96&type=FAVICON&fallback_opts=TYPE,SIZE,URL",
+      "authors": [
+        "Johnnie Bachusky"
+      ]
+    },
+    "link": "https://www.thealbertan.com/penhold-news/ford-family-scholarship-gives-out-50000-to-penhold-students-9206626",
+    "thumbnail": "https://www.vmcdn.ca/f/files/mountainviewtoday/images/mvt-penhold-crossing-secondary-school-scholarships-2024.jpg;w=960",
+    "date": "07/11/2024, 04:59 PM, +0000 UTC"
+  },
+  {
+    "position": 6,
+    "title": "Te Tai Tokerau tertiary scholarship winners named",
+    "source": {
+      "name": "Te Ao Māori News",
+      "icon": "https://lh3.googleusercontent.com/f7IxFreeEkmpFQeqHdUuzTp5XDOTN79vo_4R2GIE5gigWNsEeu3GxWm1RGdJXEMpNXsl-BlV"
+    },
+    "link": "https://www.teaonews.co.nz/2024/07/12/te-tai-tokerau-tertiary-scholarship-winners-named/",
+    "thumbnail": "https://whakaatamaori-teaomaori-prod.web.arc-cdn.net/resizer/v2/IRWLH7NCZJFO5GZ5SJNGRHSGNA.jpg?auth=b41f4d14f3863d887c15850bae7ac305e7ccd7816b3fa455774e916a715e6041&width=768",
+    "date": "07/11/2024, 09:01 PM, +0000 UTC"
+  },
+  {
+    "position": 7,
+    "title": "Menlo Park updates its recreation scholarship policy",
+    "source": {
+      "name": "The Almanac Online",
+      "icon": "https://lh3.googleusercontent.com/lJhDMzU-L07phLYEI7PStIN-kzePuk7vJpWF7V2Q-yQfSMv2Ce64ftCg_JxvDd_sHccJV348"
+    },
+    "link": "https://www.almanacnews.com/menlo-park/2024/07/11/menlo-park-updates-its-recreation-scholarship-policy/",
+    "thumbnail": "https://i0.wp.com/www.almanacnews.com/wp-content/uploads/2022/04/59536_original-1.jpg?fit=1000%2C667&ssl=1",
+    "date": "07/11/2024, 10:20 PM, +0000 UTC"
+  },
+  {
+    "position": 8,
+    "title": "Many universities are abandoning race-conscious scholarships worth millions",
+    "source": {
+      "name": "The Washington Post",
+      "icon": "https://lh3.googleusercontent.com/o6yPxQMQu9ofTcfOYO51BWEv7jK44QjB14DqTK_cl1N3LsWVEF3GUHd1byHOQ3n8L6ySZluEhZQ",
+      "authors": [
+        "Danielle Douglas-Gabriel"
+      ]
+    },
+    "link": "https://www.washingtonpost.com/education/2024/07/09/college-scholarships-race-affirmative-action/",
+    "thumbnail": "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/4VKACJN5YNEOHAVO2EOJTFJREE.jpg&w=1200",
+    "date": "07/09/2024, 05:00 PM, +0000 UTC"
+  },
+  {
+    "position": 9,
+    "title": "Retired teachers give scholarships",
+    "source": {
+      "name": "The Daily Times",
+      "icon": "https://encrypted-tbn2.gstatic.com/faviconV2?url=https://www.weirtondailytimes.com&client=NEWS_360&size=96&type=FAVICON&fallback_opts=TYPE,SIZE,URL"
+    },
+    "link": "https://www.weirtondailytimes.com/news/community/2024/07/retired-teachers-give-scholarships/",
+    "thumbnail": "https://ogden_images.s3.amazonaws.com/www.weirtondailytimes.com/images/2024/07/11144019/Retired-School-Employees-630x840.jpg",
+    "date": "07/12/2024, 06:34 AM, +0000 UTC"
+  },
+  {
+    "position": 10,
+    "title": "UCF Launches Inaugural Mentorship, Scholarship Initiative for Students in AI",
+    "source": {
+      "name": "UCF",
+      "icon": "https://encrypted-tbn0.gstatic.com/faviconV2?url=https://www.ucf.edu&client=NEWS_360&size=96&type=FAVICON&fallback_opts=TYPE,SIZE,URL",
+      "authors": [
+        "Eddy Duryea"
+      ]
+    },
+    "link": "https://www.ucf.edu/news/ucf-launches-inaugural-mentorship-scholarship-initiative-for-students-in-ai/",
+    "thumbnail": "https://www.ucf.edu/wp-content/blogs.dir/20/files/2024/07/shah-resize.jpg",
+    "date": "07/11/2024, 05:44 PM, +0000 UTC"
+  },
+  {
+    "position": 11,
+    "title": "PBA announces winners of inaugural scholarship awards",
+    "source": {
+      "name": "Bus & Motorcoach News",
+      "icon": "https://encrypted-tbn3.gstatic.com/faviconV2?url=https://www.busandmotorcoachnews.com&client=NEWS_360&size=96&type=FAVICON&fallback_opts=TYPE,SIZE,URL"
+    },
+    "link": "https://www.busandmotorcoachnews.com/pba-announces-winners-of-inaugural-scholarship-awards/",
+    "thumbnail": "https://i0.wp.com/www.busandmotorcoachnews.com/wp-content/uploads/sites/4/2024/07/Screenshot-2024-07-11-at-5.44.26%E2%80%AFPM.png?resize=660%2C383&ssl=1",
+    "date": "07/11/2024, 09:50 PM, +0000 UTC"
+  },
+  {
+    "position": 12,
+    "title": "Presidential Scholarship awards to commence at district level",
+    "source": {
+      "name": "Newsfirst.lk",
+      "icon": "https://encrypted-tbn3.gstatic.com/faviconV2?url=https://www.newsfirst.lk&client=NEWS_360&size=96&type=FAVICON&fallback_opts=TYPE,SIZE,URL"
+    },
+    "link": "https://www.newsfirst.lk/2024/07/12/presidential-scholarship-awards-to-commence-at-district-level",
+    "thumbnail": "https://cdn.newsfirst.lk/english-uploads/2024/07/president%20scholarship%20sri%20lanka%20pmd-760806.jpg",
+    "date": "07/12/2024, 06:10 AM, +0000 UTC"
+  },
+  {
+    "position": 13,
+    "title": "Allegany-Limestone grads receive Higgins, other CRCF scholarship awards",
+    "source": {
+      "name": "Olean Times Herald",
+      "icon": "https://encrypted-tbn3.gstatic.com/faviconV2?url=https://www.oleantimesherald.com&client=NEWS_360&size=96&type=FAVICON&fallback_opts=TYPE,SIZE,URL",
+      "authors": [
+        "Kellen Quigley"
+      ]
+    },
+    "link": "https://www.oleantimesherald.com/news/cattaraugus_county/allegany-limestone-grads-receive-higgins-other-crcf-scholarship-awards/article_c6be8792-3f9e-11ef-a2f8-532a66b8aa6d.html",
+    "thumbnail": "https://bloximages.chicago2.vip.townnews.com/oleantimesherald.com/content/tncms/assets/v3/editorial/e/5b/e5bec2a6-3f9e-11ef-9725-13937dc40fe0/6690020755bd1.image.jpg?resize=400%2C111",
+    "date": "07/11/2024, 09:00 PM, +0000 UTC"
   }
-}
+]
 
-const Articles = () => {
-  const [articles, setArticles] = useState<Article[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    const getArticles = async () => {
-      setLoading(true);
-      setError(false);
-      const fetchedArticles = await fetchNews();
-      setArticles(fetchedArticles);
-      setLoading(false);
-    };
-
-    getArticles();
-  }, []);
-
+export default function Articles() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-7xl">
-        <h2 className="text-2xl font-bold mb-6 text-center">Articles</h2>
-        {loading && <p className="text-center">Loading...</p>}
-        {error && (
-          <p className="text-center text-red-500">Error loading articles</p>
-        )}
-        {!loading && !error && (
-          <ul>
-            {articles.map((article, index) => (
-              <li key={index} className="mb-4">
-                <a href={article.link} className="text-blue-500 font-semibold">
-                  {article.title}
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Learn how to grow your business with our expert advice.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post) => (
+            <article
+              key={post.position}
+              className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
+            >
+              <img alt="" src={post.thumbnail} className="absolute inset-0 -z-10 h-full w-full object-cover" />
+              <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
+              <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+
+              <div className="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
+                <time dateTime={post.date} className="mr-8">
+                  {post.date}
+                </time>
+                <div className="-ml-4 flex items-center gap-x-4">
+                  <svg viewBox="0 0 2 2" className="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
+                    <circle r={1} cx={1} cy={1} />
+                  </svg>
+                  <div className="flex gap-x-2.5">
+                    <img alt="" src={post.source.icon} className="h-6 w-6 flex-none rounded-full bg-white/10" />
+                    {post.source.name}
+                  </div>
+                </div>
+              </div>
+              <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
+                <a target="_blank" href={post.link} >
+                  <span className="absolute inset-0" />
+                  {post.title}
                 </a>
-                <p>{article.snippet}</p>
-              </li>
-            ))}
-          </ul>
-        )}
+              </h3>
+            </article>
+          ))}
+        </div>
       </div>
     </div>
-  );
-};
-
-export default Articles;
+  )
+}
