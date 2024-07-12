296c56fc9973462077ff1a9945422c01438e58bd
iqbal <13520035@std.stei.itb.ac.id>
2024-07-12 22:04:49 +0700

    fix: comment checkbox animation

--- frontend/src/components/Reminder.tsx
+++ frontend/src/components/Reminder.tsx
@@ -13,10 +13,13 @@ const Reminder: React.FC<ReminderProps> = ({
   remainingTasks,
 }) => {
   return (
-    <div className="flex flex-row items-center justify-between">
-      <div className="w-64 h-64 mr-4 flex">
+    <div className="flex flex-col items-center mt-2">
+      {/* <div
+        className="w-16 h-16 mr-4 flex ml-12 mt-4"
+        style={{ transform: "scale(2)" }}
+      >
         <Lottie animationData={progressAnimation} loop={true} />
-      </div>
+      </div> */}
       <div className="bg-blue-100 p-4 mt-4 rounded-lg shadow-lg flex items-center">
         <CalendarIcon className="text-blue-500 h-6 w-6 mr-2" />
         <CheckCircleIcon className="text-green-500 h-6 w-6 mr-2" />
