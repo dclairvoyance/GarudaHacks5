2c818d58402afe22f67c4ec4fac672ac3110034d
pandora-1 <michaelkahari.mk@gmail.com>
2024-07-12 19:25:28 +0700

    feat: number commas

--- frontend/src/components/BussinessStats.tsx
+++ frontend/src/components/BussinessStats.tsx
@@ -15,7 +15,12 @@ const StatsCard: React.FC<StatsCardProps> = ({ title, value, icon }) => {
       delay: 200,
       config: { mass: 1, tension: 20, friction: 10 },
     });
-    return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
+    return <animated.div>
+      {number.to((n) => new Intl.NumberFormat('en-US', {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+      }).format(n))}
+    </animated.div>;
   }
   return (
     <div className="flex items-center space-x-4">
